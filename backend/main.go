619b30b5b5b50ac44b5ad2e7c7285c579c3ab876
andipyk <Andiokey123>
2020-08-16 03:37:32 +0700

    add front-end (svelte) and make CORSWithConfig

--- backend/main.go
+++ backend/main.go
@@ -3,11 +3,26 @@ package main
 import (
 	"college-with-ui/backend/routes"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 
 	e := routes.Index()
+
+	// Middleware
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	})) // logger
+
+	e.Use(middleware.Recover()) // recover
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+	})) // CORS
 
 	e.Logger.Fatal(e.Start(":3000"))
 }
---
+++ frontend/src/Mahasiswa.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let mhs;
+</script>
+
+<style>
+
+</style>
+
+<div>
+    <h1>{mhs.nama}</h1>
+    <small>
+        NIM: <b>{mhs.nim}</b>		
+    </small>
+</div>
